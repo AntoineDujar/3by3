f5cfbb79c7b559ebb2f38e0fc46f57dd420e3cda
AntoineDujar <antoine.v.dujardin@gmail.com>
2024-03-09 17:50:43 -0500

    added example usernames

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,16 @@ function App() {
   const [username, setUsername] = useState("");
 
   const divStyle = {
-    display: 'flex',
-    flexDirection: 'column',
-    minWidth: '40rem',
-    alignItems: 'center'
-  }
+    display: "flex",
+    flexDirection: "column",
+    minWidth: "40rem",
+    alignItems: "center",
+  };
 
   return (
     <div style={divStyle}>
       <h3>Anilist 3by3 recent favourites</h3>
+      <p>example usernames: KorudoKohi, jabjibjam</p>
       <Input setUsername={setUsername} />
       <Request username={username} />
     </div>
